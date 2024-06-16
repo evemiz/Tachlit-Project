063a0a71be36116fd9321d4363da229010c2ccb6
TalSaraDaniel <Tal.Levi.d84@gmail.com>
2024-06-16 16:15:28 +0300

    Admin

--- src/Admin/SignUpNewAdmin.js
+++ src/Admin/SignUpNewAdmin.js
@@ -1,5 +1,46 @@
-function SignUp () {
+import React, { useState } from "react";
+import {auth} from "../firebaseConfig"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+function SignUp () {
+    const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("User registered:", user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Error registering user:", errorCode, errorMessage);
+      });
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSignUp}>
+        <input
+          type="email"
+          placeholder="שם משתמש"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          dir = "rtl"
+        />
+        <input
+          type="password"
+          placeholder="סיסמה"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          dir = "rtl"
+        />
+        <button type="submit">הוספת מנהל חדש</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
--- src/Admin/login.js
+++ src/Admin/login.js
@@ -1,5 +1,54 @@
-function Login () {
+import React, { useState } from "react";
+import { auth } from "../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigate('/AdminMain');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Error logging in user:", errorCode, errorMessage);
+        setMessage("Login failed. Please check your credentials and try again.");
+      });
+
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="שם משתמש"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          dir="rtl"
+        />
+        <input
+          type="password"
+          placeholder="סיסמה"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          dir="rtl"
+        />
+        <button type="submit">התחבר</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
 }
 
 export default Login;
