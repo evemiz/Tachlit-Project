be98360bff5ac4da603483eba9c762235c29fbf4
TalSaraDaniel <Tal.Levi.d84@gmail.com>
2024-06-23 18:38:28 +0300

    work include login

--- src/Admin/login.js
+++ src/Admin/login.js
@@ -1,117 +1,54 @@
-
-
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { signOut, reauthenticateWithCredential, EmailAuthProvider, updatePassword } from "firebase/auth";
 import { auth } from "../firebaseConfig";
-import Modal from 'react-modal';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
-Modal.setAppElement('#root');
 
-function AdminMain() {
-  const navigate = useNavigate();
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+function Login() {
   const [email, setEmail] = useState("");
-  const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-  const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        console.log("User logged out");
-        navigate("/login");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigate('/AdminMain');
       })
       .catch((error) => {
-        console.error("Error logging out:", error);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Error logging in user:", errorCode, errorMessage);
+        setMessage("Login failed. Please check your credentials and try again.");
       });
-  };
 
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setEmail("");
-    setOldPassword("");
-    setNewPassword("");
-    setMessage("");
-  };
-
-  const handleChangePassword = (e) => {
-    e.preventDefault();
-    const user = auth.currentUser;
-
-    if (user && user.email === email) {
-      const credential = EmailAuthProvider.credential(email, oldPassword);
-      reauthenticateWithCredential(user, credential)
-        .then(() => {
-          updatePassword(user, newPassword)
-            .then(() => {
-              setMessage("הסיסמה שונתה בהצלחה");
-              closeModal();
-            })
-            .catch((error) => {
-              console.error("Error updating password:", error);
-              setMessage("שגיאה בעדכון הסיסמה");
-            });
-        })
-        .catch((error) => {
-          console.error("Error reauthenticating user:", error);
-          setMessage("שגיאה באימות המשתמש. נא לבדוק את הסיסמה הישנה.");
-        });
-    } else {
-      setMessage("האימייל שהוזן אינו תואם את האימייל של המשתמש המחובר");
-    }
   };
 
   return (
     <div className="App">
-      <h1>דף מנהל</h1>
-      <button>
-        <Link to="/Lists">Go to Lists</Link>
-      </button>
-      <button>
-        <Link to="/SignUp">Go to SignUp</Link>
-      </button>
-      <button onClick={handleLogout}>התנתקות</button>
-      <button onClick={openModal}>שינוי סיסמה</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Change Password Modal"
-      >
-        <h2>שינוי סיסמה</h2>
-        <form onSubmit={handleChangePassword}>
-          <input
-            type="email"
-            placeholder="אימייל"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            dir="rtl"
-          />
-          <input
-            type="password"
-            placeholder="סיסמה ישנה"
-            value={oldPassword}
-            onChange={(e) => setOldPassword(e.target.value)}
-            dir="rtl"
-          />
-          <input
-            type="password"
-            placeholder="סיסמה חדשה"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-            dir="rtl"
-          />
-          <button type="submit">שנה סיסמה</button>
-        </form>
-        {message && <p>{message}</p>}
-        <button onClick={closeModal}>סגור</button>
-      </Modal>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="שם משתמש"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          dir="rtl"
+        />
+        <input
+          type="password"
+          placeholder="סיסמה"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          dir="rtl"
+        />
+        <button type="submit">התחבר</button>
+      </form>
+      {message && <p>{message}</p>}
     </div>
   );
 }
 
-export default AdminMain;
+export default Login;
