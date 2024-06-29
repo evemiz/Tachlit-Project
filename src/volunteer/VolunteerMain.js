76433f99a9aee3433adddaacecbbbd5227375e8b
TalSaraDaniel <Tal.Levi.d84@gmail.com>
2024-06-29 16:54:05 +0300

    desigen

--- src/volunteer/VolunteerMain.js
+++ src/volunteer/VolunteerMain.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import CloseRequest from '../Messages/CloseRequest';
+import { Link, useNavigate } from "react-router-dom";
+
+
 
 function VolunteerMain() {
   return (
     <div>
       <h1>דף ראשי מתנדב</h1>
+      <button>
+        <Link to="/CloseRequest">Go to CloseRequest</Link>
+      </button>
+      <button>
+        <Link to="/GetFeedback">Go to GetFeedback</Link>
+      </button>
+      <button>
+        <Link to="/ViewRequest">Go to ViewRequest</Link>
+      </button>
     </div>
   );
 }
